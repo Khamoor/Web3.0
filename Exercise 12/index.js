a05b69d17d9e58ad1047b1ea6cc3215e69f4d929
Khamoor <mohammedkhamoor@gmail.com>
2022-10-04 18:11:35 +0500

    Exercise 12 Complete

---
+++ Exercise 12/index.js
@@ -0,0 +1,5 @@
+var car = ["Honda Civic", "Toyota Corolla", "BMW", "Ferrari"];
+alert(car[3] + " is the fastest car.");
+alert("Its my dream to drive " + car[2] + ".");
+alert("This car is mostly used " + car[0] + ".");
+alert(car[1] + " looks is not attractive.");

93d07e8ab8b9fcdac69238cd86dd505352bedf49
Khamoor <mohammedkhamoor@gmail.com>
2022-10-04 21:00:00 +0500

    Exercise 17 Complete

---
+++ Exercise 17/index.js
@@ -0,0 +1,15 @@
+var locations = ["Paris", "Egypt", "London", "Australia", "Canada"];
+alert(locations);
+var alphabetical_sort = locations.slice().sort()
+alert(alphabetical_sort);
+alert(locations);
+alert(alphabetical_sort.reverse());
+alert(locations);
+var reverse_locations = locations.reverse()
+alert(reverse_locations);
+alert(reverse_locations.reverse());
+var alphabetical_sort = locations.slice().sort();
+alert(alphabetical_sort);
+var reverse_alphabetical_sort = alphabetical_sort.reverse();
+alert(reverse_alphabetical_sort);
+

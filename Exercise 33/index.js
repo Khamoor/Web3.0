1540dad30c165cc825108b2e46f40c153a6954d8
Khamoor <mohammedkhamoor@gmail.com>
2022-10-09 14:02:07 +0500

    Exercise 33 Complete

---
+++ Exercise 33/index.js
@@ -0,0 +1,11 @@
+var pizzas = ["Cheese Pizza", "Pepperoni Pizza", "BBQ Chicken Pizza"];
+for (var i = 0; i < pizzas.length; i ++) {
+    console.log(pizzas[i]);
+}
+console.log("\n");
+for (var i = 0; i < pizzas.length; i ++) {
+    console.log("I like " + pizzas[i] + ".");
+}
+console.log("There are various Kinds of Pizza.")
+console.log("I like Cheese & BBQ Chicken Pizza.")
+console.log("I really love Pizza!")

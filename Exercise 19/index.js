27903bc9e9e39cf86919c2ef85726d3c263e1045
Khamoor <mohammedkhamoor@gmail.com>
2022-10-09 08:44:36 +0500

    Exercise 19 Complete

---
+++ Exercise 19/index.js
@@ -0,0 +1,8 @@
+var Mountains = ["Mount Everest", "K-2", "Nanga Parbat"];
+var Rivers = ["Nile", "Jhelum", "Indus", "Amazon"];
+var Countries = ["Pakistan", "Australia", "England", "New Zealand"];
+var Cities = ["Karachi", "Dubai", "London", "Paris"];
+var Languages = ["Urdu", "Chinese", "Arabic", "English"];
+
+alert("Mountains: " + Mountains + "\n" + "Rivers: " + Rivers + "\n" + "Countries: " + Countries
+ + "\n" + "Cities: " + Cities + "\n" + "Languages: " + Languages);

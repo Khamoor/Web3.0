4ce03f7908fb09f87802d653dae53bafb2227d8a
Khamoor <mohammedkhamoor@gmail.com>
2022-10-09 08:59:10 +0500

    Exercise 20 Complete

---
+++ Exercise 20/index.js
@@ -0,0 +1,5 @@
+const obj = {Mountain : "Mount Everest", River : "Nile", Country : "Pakistan", City : "Karachi", 
+Language : "English"};
+
+alert("Mountains: " + obj.Mountain + "\n" + "Rivers: " + obj.River + "\n" + "Countries: " + obj.Country
+ + "\n" + "Cities: " + obj.City + "\n" + "Languages: " + obj.Language);

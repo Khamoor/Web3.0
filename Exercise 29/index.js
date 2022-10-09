1c87aaa78c91aad932bbdc0348cb8fe368661b45
Khamoor <mohammedkhamoor@gmail.com>
2022-10-09 12:03:56 +0500

    Exercise 29 Complete

---
+++ Exercise 29/index.js
@@ -0,0 +1,10 @@
+var user_names = ["Habib", "Hassan", "Admin", "Zain", "Umer", "Abdullah"];
+
+for (var i = 0; i < user_names.length; i++) {
+    if (user_names[i] === "Admin") {
+        console.log("Hello! " + user_names[i] + ", would you like to see a status Report?");
+    }
+    else {
+        console.log("Hello! " + user_names[i] + ", thank you for logging in Again");
+    }
+}

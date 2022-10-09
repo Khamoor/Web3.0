06bf4827e9f7f5212103c5d794e50ff0918ed535
Khamoor <mohammedkhamoor@gmail.com>
2022-10-09 09:41:10 +0500

    Exercise 22 Complete

---
+++ Exercise 22/index.js
@@ -0,0 +1,23 @@
+let name = "Habib";
+console.log("Is name == 'Habib'? I predict 'True'");
+console.log(name == "Habib");
+console.log("Is name == 'habib'? I predict 'False'");
+console.log(name == "habib");
+
+let num = 5;
+console.log("Is num == '5'? I predict 'True'");
+console.log(num == '5');
+console.log("Is num === '5'? I predict 'False'");
+console.log(num === '5');
+console.log("Is num == 5? I predict 'True'");
+console.log(num == 5);
+console.log("Is num == 6? I predict 'False'");
+console.log(num == 6);
+console.log("Is num > 3? I predict 'True'");
+console.log(num > 3);
+console.log("Is num > 8? I predict 'False'");
+console.log(num > 8);
+console.log("Is num < 8? I predict 'True'");
+console.log(num < 8);
+console.log("Is num < 3? I predict 'False'");
+console.log(num < 3);

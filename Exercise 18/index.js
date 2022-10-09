eb95ac01f34ffb8bf6c3c449efe16517bdc64c55
Khamoor <mohammedkhamoor@gmail.com>
2022-10-09 08:20:12 +0500

    Exercise 18 Complete

---
+++ Exercise 18/index.js
@@ -0,0 +1,5 @@
+var guests = ["Huzaifa", "Abdul Bari", "Kamran"];
+alert("I've Invited " + guests.length + " number of Guests to Dinner.");
+for (var i = 0; i < guests.length; i++) {
+    alert("My Friend " + guests[i] + " you are invited to Dinner.");
+}

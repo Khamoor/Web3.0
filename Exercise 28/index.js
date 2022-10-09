19ef58eb39bc303f3e2b5781895d65bbdae51316
Khamoor <mohammedkhamoor@gmail.com>
2022-10-09 11:57:26 +0500

    Exercise 28 Complete

---
+++ Exercise 28/index.js
@@ -0,0 +1,17 @@
+var favorite_fruits = ["Apple", "Banana", "Orange"];
+
+if (favorite_fruits.includes("Apple")) {
+    console.log("You Really Like Apples!");
+}
+if (favorite_fruits.includes("Banana")) {
+    console.log("You Really Like Bananas!");
+}
+if (favorite_fruits.includes("Orange")) {
+    console.log("You Really Like Oranges!");
+}
+if (favorite_fruits.includes("Grapes")) {
+    console.log("You Really Like Grapes!");
+}
+if (favorite_fruits.includes("Strawberry")) {
+    console.log("You Really Like Strawberries!");
+}

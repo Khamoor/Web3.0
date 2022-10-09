87425ba8a54f555bc8deb004e7bee14b866621d1
Khamoor <mohammedkhamoor@gmail.com>
2022-10-09 14:19:13 +0500

    Exercise 34 Complete

---
+++ Exercise 34/index.js
@@ -0,0 +1,14 @@
+var pets = ["Cat", "Dog", "Fish"];
+for (var i = 0; i < pets.length; i ++) {
+    console.log(pets[i]);
+}
+console.log("\n");
+for (var i = 0; i < pets.length; i ++) {
+    console.log("A " + pets[i] + " would make a great pet.");
+}
+
+// console.log("Cats are Intelligent.")
+// console.log("Dogs are loyal to Human")
+// console.log("Fish are great stress-reliever.")
+console.log("\n")
+console.log("Any of these Animals would make a great pet!")

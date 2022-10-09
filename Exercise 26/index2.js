efc513098c0088d46c40e39fa0792eefca2b5b2e
Khamoor <mohammedkhamoor@gmail.com>
2022-10-09 11:30:45 +0500

    Exercise 26 Complete

---
+++ Exercise 26/index2.js
@@ -0,0 +1,11 @@
+var alien_color = "yellow";
+
+if (alien_color === "green") {
+    console.log("Player just Earned 5 points.")
+}
+else if (alien_color === "yellow") {
+    console.log("Player just Earned 10 points.")
+}
+else if (alien_color === "red") {
+    console.log("Player just Earned 15 points.")
+}

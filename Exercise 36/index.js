12d51fc1966b0be8594eddc069f9943af1bfb8f9
Khamoor <mohammedkhamoor@gmail.com>
2022-10-16 07:44:09 +0500

    Exercise 36 Complete

---
+++ Exercise 36/index.js
@@ -0,0 +1,6 @@
+function make_shirt(size = "Large", text_message = "I love JavaScript") {
+    console.log("The size of Shirt is " + size + '. And the Message is "' + text_message + '".')
+}
+make_shirt();
+make_shirt("Medium");
+make_shirt("X-Large", "Hello, World!");

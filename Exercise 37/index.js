c6a46e2ed6a48fdcc27922fe6fb9dcdf4018911f
Khamoor <mohammedkhamoor@gmail.com>
2022-10-16 07:54:01 +0500

    Exercise 37 Complete

---
+++ Exercise 37/index.js
@@ -0,0 +1,6 @@
+function describe_city(name_city, name_country = "Pakistan") {
+    console.log(name_city + ' is in ' + name_country);
+}
+describe_city("Karachi");
+describe_city("Lahore");
+describe_city("Birmingham", "England");

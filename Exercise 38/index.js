ba7502cef38398f4ec68a97db729fc2506aba04e
Khamoor <mohammedkhamoor@gmail.com>
2022-10-16 08:00:44 +0500

    Exercise 38 Complete

---
+++ Exercise 38/index.js
@@ -0,0 +1,6 @@
+function city_country(name_city, name_country) {
+    return '"' + name_city + ", " + name_country + '"'
+}
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("New York", "America"));
+console.log(city_country("London", "England"));

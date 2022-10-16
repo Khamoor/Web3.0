18a21243c438c691d1be3c3682ca99ab5e095e1c
Khamoor <mohammedkhamoor@gmail.com>
2022-10-16 08:48:32 +0500

    Exercise 40 Complete

---
+++ Exercise 40/index.js
@@ -0,0 +1,7 @@
+var magicians = ["David Copperfield", "Ricky Jay", "Mark Wilson", "Harry Anderson"];
+function show_magicians(names) {
+    for (var i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
+}
+show_magicians(magicians);
